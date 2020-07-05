556f03a1b1188bafa4a3ca23d9f7dcfbcddd4e5e
parvizi <ali.reza.parvizi.mosaed@gmail.com>
2020-07-04 19:21:31 -0700

    add sdk config file

---
+++ sdkconfig/setup.go
@@ -0,0 +1,39 @@
+package sdkconfig
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric-sdk-go/pkg/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+
+// FabricSetup implementation
+type FabricSetup struct {
+	ConfigFile      string
+//	ChannelID       string
+	initialized     bool
+//	ChannelConfig   string
+	OrgAdmin        string
+	OrgName         string
+	admin           resmgmt.ResourceMgmtClient
+	sdk             *fabsdk.FabricSDK
+}
+
+// Initialize reads the configuration file and sets up the client, chain and event hub
+func (setup *FabricSetup) Initialize() error {
+
+	// Add parameters for the initialization
+	if setup.initialized {
+		return fmt.Errorf("sdk already initialized")
+	}
+
+	// Initialize the SDK with the configuration file
+	sdk, err := fabsdk.New(config.FromFile(setup.ConfigFile))
+	if err != nil {
+		return fmt.Errorf("failed to create sdk: %v", err)
+	}
+	setup.sdk = sdk
+
+	fmt.Println("Initialization Successful")
+	setup.initialized = true
+	return nil
+}
